fcd7e20e0d8f5cc7e171b065d49252f7920bf701
Bekzoddeveloper <bekzodrakhmonov1995@gmail.com>
2022-06-14 12:29:44 +0500

    last changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
-import { persistor,store } from './store/index';
+import { persistor, store } from './store/index';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import i18n from "./lang/index.js";
@@ -15,9 +15,9 @@ root.render(
     <I18nextProvider i18n={i18n} >
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-            <App />
+          <App />
         </PersistGate>
-       </Provider>
+      </Provider>
     </I18nextProvider>
   </React.StrictMode>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { useSelector,useDispatch } from "react-redux";
-import { getCountCol,deleteCountCol } from "../store/slices/currency";
+import { useSelector, useDispatch } from "react-redux";
+import { getCountCol, deleteCountCol } from "../store/slices/currency";
 import CurrencyForm from "../components/CurrencyForm";
 import DeleteBtn from "../components/DeleteBtn";
 
@@ -9,35 +9,35 @@ const Home = () => {
   const colCount = useSelector(state => state.currency.count_col)
   const dispatch = useDispatch()
 
-  const addCol= () => {
+  const addCol = () => {
     dispatch(getCountCol())
   }
-  const deleteCol = (e) =>  {
+  const deleteCol = (e) => {
     console.log(e.currentTarget.dataset.id);
-      dispatch(deleteCountCol(e.currentTarget.dataset.id))
+    dispatch(deleteCountCol(e.currentTarget.dataset.id))
   }
 
-  return <> 
+  return <>
     <div className="home">
-        {
-          colCount.map((item,index) => (
-            <div className="currency_form_wrapper">
-              <span className="delete_icon_span">
-                <DeleteBtn deleteItem={(e) => deleteCol(e)} dataset={index} />
-              </span>
-              <CurrencyForm id={index + 1} key={item}/>
-            </div>
-          ))
-        }
-         <svg
-              className="add_btn"
-              onClick={addCol}
-              width="35" height="35"
-              viewBox="0 0 48 48"><path fill="#4caf50" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z">
-                </path><path fill="#fff" d="M21,14h6v20h-6V14z"></path><path fill="#fff" d="M14,21h20v6H14V21z"></path>
+      {
+        colCount.map((item, index) => (
+          <div className="currency_form_wrapper" key={index}>
+            <span className="delete_icon_span">
+              <DeleteBtn deleteItem={(e) => deleteCol(e)} dataset={index} />
+            </span>
+            <CurrencyForm id={index + 1} key={item} />
+          </div>
+        ))
+      }
+      <svg
+        className="add_btn"
+        onClick={addCol}
+        width="35" height="35"
+        viewBox="0 0 48 48"><path fill="#4caf50" d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z">
+        </path><path fill="#fff" d="M21,14h6v20h-6V14z"></path><path fill="#fff" d="M14,21h20v6H14V21z"></path>
       </svg>
     </div>
-   </>
+  </>
 }
 
 export default Home;
